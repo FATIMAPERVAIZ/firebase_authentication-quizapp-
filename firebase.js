8d0fccb58da34f29789912ccc9da899ca24dd5af
FATIMAPERVAIZ <158159875+FATIMAPERVAIZ@users.noreply.github.com>
2026-04-21 14:36:00 +0500

    Clear Firebase configuration values

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword ,onAuthStateChanged, sendEmailVerification, signOut,signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore, collection, addDoc,getDocs ,doc, setDoc,updateDoc,serverTimestamp , arrayUnion, arrayRemove ,deleteDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"
 const firebaseConfig = {
-  apiKey: "AIzaSyCBj61ozUOQYwyOeDrYitLElAzUaK3Kzfk",
-  authDomain: "myfirst-webapp.firebaseapp.com",
-  projectId: "myfirst-webapp",
-  storageBucket: "myfirst-webapp.firebasestorage.app",
-  messagingSenderId: "862813763911",
-  appId: "1:862813763911:web:08072b25920d1bd921b462",
-  measurementId: "G-LFC2EYVYQQ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
